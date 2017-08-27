d5f0ba24ef94d9933c52e9113eeaf334584d22f9
Aaronnichols2025 <aaronnichols2025@gmail.com>
2017-08-26 20:03:53 -0500

    Updated Harvey

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const token = 'your bot token here';
+
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!') ;
-  }
-});
+
+client.on('guildMemberAdd', member => {
+ 
+  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+
+
+  const channel = member.guild.channels.find('name', 'member-log');
+  
+   if (!channel) return;
+  
+   channel.send(`Welcome to the server, ${member}`);
+    
+  });
+ 
+
+client.on('guildMemberRemove', member => {
+ 
+  member.guild.defaultChannel.send(`Bye bye, ${member}!`);
+
+
+  const channel = member.guild.channels.find('name', 'member-log');
+  
+   if (!channel) return;
+  
+   channel.send(`Bye bye, ${member}`);
+    
+  });
+
 
 client.login('MzUwODU3MDk0NDM1NzAwNzM3.DIKRsA.V8SL8-GowxQCLuO2XFyTx0rpmDs');
 
